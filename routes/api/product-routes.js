a4ab602357b01a5b37ee59f0a149bb79739bf1c7
Wave FC <malloriepinto89@gmail.com>
2023-06-30 21:37:28 -0700

    updated product and category routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,18 +4,57 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [
+        Category,
+        {
+          model: Tag, 
+          through: ProductTag
+        }
+      ]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findone({
+      where: {
+        id:req.params.id
+      }, 
+      include: [
+        Category,
+        {
+          model: Tag, 
+          through: ProductTag
+        }
+      ]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
-// create new product
+// create new product NO ACTION NEEDED?
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
@@ -92,8 +131,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-});
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
 
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
